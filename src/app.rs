d8b8fe48d23ff5dd9af958e3a3c254728b4fa817
sor-ca <mariagrandova@gmail.com>
2023-07-15 09:36:59 +0300

    show current time

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,80 @@
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+use time::{Time, OffsetDateTime};
+
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct TemplateApp {
+    time: Time,
+}
+
+impl Default for TemplateApp {
+    fn default() -> Self {
+        Self {
+            time: OffsetDateTime::now_local().unwrap_or(OffsetDateTime::now_utc()).time(),
+        }
+    }
+}
+
+impl TemplateApp {
+    
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+
+        Default::default()
+    }
+}
+
+impl eframe::App for TemplateApp {
+    /// Called by the frame work to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let Self { time } = self;
+
+        // Examples of how to create different panels and windows.
+        // Pick whichever suits you.
+        // Tip: a good default choice is to just keep the `CentralPanel`.
+        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        #[cfg(not(target_arch = "wasm32"))] // no File->Quit on web pages!
+        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            // The top panel is often a good place for a menu bar:
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {
+                    if ui.button("Quit").clicked() {
+                        _frame.close();
+                    }
+                });
+            });
+        });
+
+        egui::SidePanel::left("side_panel").show(ctx, |ui| {
+            ui.heading("What time is it now?");
+
+            ui.horizontal(|ui| {
+                ui.label(format!("{}", &time.hour()));
+                ui.label(" : ");
+                ui.label(format!("{}", &time.minute()));
+            });
+
+            if ui.button("time now").clicked() {
+                *time = OffsetDateTime::now_local().unwrap_or(OffsetDateTime::now_utc()).time();
+            }
+
+        });
+
+    }
+}
+
+
+
+/*/// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
@@ -113,4 +189,4 @@ impl eframe::App for TemplateApp {
             });
         }
     }
-}
+}*/
